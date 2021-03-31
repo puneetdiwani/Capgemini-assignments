f1df2946d06daec4b784a81e1faaf1c918d61bc0
puneetdiwani <49617979+puneetdiwani@users.noreply.github.com>
2021-03-31 11:57:36 +0530

    Add files via upload

---
+++ JAVA 8/assignment 2/Q6.java
@@ -0,0 +1,26 @@
+abstract class Persistence {
+    abstract public String persist();
+}
+class FilePersistence extends Persistence{
+    @Override
+    public String persist() {
+    	return "Available";
+    }
+}
+class DatabasePersistence extends Persistence{
+    @Override
+    public String persist() {
+    	return "Available";
+    }
+}
+//Cannot make object of Persistence, because it is a abstract class.
+class client extends Persistence{
+	public String persist() {
+    	return "Available";
+    }
+}
+
+
+public class Assignment2Q6 {
+    public static void main(String[] args) {}
+}
